6e5057a696c086926e08fa56d56f08bb8836abd0
probit @ 3520 <probitlabs@protonmail.com>
2018-03-05 06:23:15 +0100

    beyond: Durin typin CLI processin

--- pkg/@beyond/src/cli.ts
+++ pkg/@beyond/src/cli.ts
@@ -1,5 +1,26 @@
-export default function main(
-    
-) {
+import {
+    Readable,
+    Writable,
+} from "stream"
 
+export const HEX_STR :unique symbol =
+    Symbol("<hexademical string>")
+
+export type Format = typeof HEX_STR
+
+export namespace flg {
+    export class Bluepr {
+         format :Format = HEX_STR
+    }
+    export interface Inter extends Bluepr {}
+    export type Flags = Partial<Inter>
+}
+
+export function run(
+    input :Readable,
+    output :Writable,
+    flags :flg.Flags = {},
+) {
+    const flags2 = {...flags} as flg.Bluepr
+    Reflect.setPrototypeOf(flags2, flg.Bluepr.prototype)
 }
