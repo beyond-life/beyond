ae06d38ba797f5a5c8237ca28682ff7be52e4c6f
probit @ 3520 <probitlabs@protonmail.com>
2018-03-06 09:23:42 +0100

    beyond: Stubbin `stdin` consumption, Loggin

--- pkg/@beyond/src/exec.ts
+++ pkg/@beyond/src/exec.ts
@@ -26,6 +26,28 @@ export namespace flg {
     export type Flags = Partial<Inter>
 }
 
+type FoldChunkL = [string, boolean]
+function foldChunk$(
+    [
+        lAcc, // left accumulator
+        lNlned, // is left newlined?
+    ] :FoldChunkL,
+    r :Buffer,
+) :FoldChunkL {
+    const rUtf = r.toString("utf-8")
+    const rNlned = rUtf.endsWith("\n")
+
+    if (rNlned) console.error("> Newline detected…")
+    else console.error("> Chunk consumed…")
+    
+    return [
+        lNlned
+            ? rUtf
+            : lAcc + rUtf,
+        rNlned,
+    ]
+}
+
 export default function exec(
     input :Readable,
     output :Writable,
@@ -33,5 +55,18 @@ export default function exec(
 ) {
     const flags2 = proto(flags, flg.Bluepr)
 
-    const data$ = fromEv(input, "data")
+    input.resume()
+
+    const chunk$ = fromEv(input, "data")
+    const chunkSeed :FoldChunkL = ["", false]
+    const data$ = chunk$
+        .fold(foldChunk$, chunkSeed)
+        .filter(([_, nlned] :FoldChunkL) :boolean => nlned)
+        .map(([acc, _] :FoldChunkL) :string => acc)
+
+    data$.subscribe({
+        next: (acc)=> console.error("==> " + acc),
+        error: (err)=> console.error("> " + err.message),
+        complete: ()=> console.error("> Completed!"),
+    })
 }
