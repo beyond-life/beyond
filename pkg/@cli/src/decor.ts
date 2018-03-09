7beded1e8f11a60fed6eddf927aa74dcb8f0ba32
probit @ 3520 <probitlabs@protonmail.com>
2018-03-09 12:46:39 +0100

    cli.decor: Aliasin `PropertyDescriptor` → `PropDesc`, Untypoin

--- pkg/@cli/src/decor.ts
+++ pkg/@cli/src/decor.ts
@@ -23,6 +23,7 @@ const {INITIAL} = Data
 
 // ~~~
 
+export type PropDesc = PropertyDescriptor
 export type PropDecor = (
     tgt :Object, //target
     prop :Prop, //property
@@ -58,7 +59,7 @@ export function alias(
             ? aliG
             : [aliG]
 
-    return (tgt :Object, prop :Prop, desc :PropertyDescriptor) :PropertyDescriptor => {
+    return (tgt :Object, prop :Prop, desc :PropDesc) :PropDesc => {
         const oldAli = hasMetadata(form, tgt, prop)
             ? getMetadata(form, tgt, prop) as string[]
             : []
@@ -70,7 +71,7 @@ export function alias(
                         isStr(prop)
                             ? prop[0] //** automagic **
                             : (() => {
-                                throw new Error("@initial: Magician died!")
+                                throw new Error("@alias: Magician died!")
                             })()
                     ])
                 : ali
@@ -87,7 +88,7 @@ export function alias(
 export function inital<Init extends Ty.NativeVal>(
     init :Init | typeof AUTOM = AUTOM,
 ) :PropDecor {
-    return (tgt :Object, prop :Prop, desc :PropertyDescriptor) :PropertyDescriptor => {
+    return (tgt :Object, prop :Prop, desc :PropDesc) :PropDesc => {
         defMd(
             INITIAL,
             AUTOM === init
