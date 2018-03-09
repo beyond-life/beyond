aab7d570ee8329d26d1c7831be8dd1fdfbb2ae62
probit @ 3520 <probitlabs@protonmail.com>
2018-03-09 13:40:30 +0100

    cli.preparse: Intin by modularizin ← `./meta`

--- pkg/@cli/src/meta.ts
+++ pkg/@cli/src/meta.ts
@@ -26,61 +26,9 @@ export interface PostO {
     [key :string] :any[]
 }
 
-namespace Env {
-    export const {SHORT} = SyxForm
-    export const {DASH_DASH} = Cmd
-    export const {SY_DATA} = Data
-
-    export type Uq = never
-        | typeof SHORT
-        | typeof DASH_DASH
-        | typeof SY_DATA
-}
-
-interface Reduc {
-    tail :string
-    env :Env.Uq
-}
-
 export function parse(
     decls :Arg[],
 ) {
     return (args :string[]) :PostO[] => {
-        args.reduce(
-            (l :Reduc, r): Reduc => {
-                const {startsWith, indexOf} = r
-                const i = indexOf(" ") as Int
-
-                function mkRelShift() :{
-                    shift :Int[]
-                } {
-                    if (l.env === SyxForm.SHORT)
-                        return {
-                            shift: [0 as Int, 1 as Int],
-                        }
-                    else
-                    if (startsWith("--")) {
-                        const lShift = 2 as Int
-                        return {
-                            shift: [lShift, i - lShift as Int],
-                        }
-                    } else {
-                        const lShift = 1 as Int
-                        return {
-                            shift: [0 as Int, 9 as Int],
-                        }
-                    }
-                }
-
-                const {shift} = mkRelShift()
-                const content = r.substr(shift[0], shift[1])
-            },
-            {
-                tail: args,
-                env: Env.SY_DATA,
-            },
-        )
-
-        return {} as {}
     }
 }
--- pkg/@cli/src/preparse.ts
+++ pkg/@cli/src/preparse.ts
@@ -12,12 +12,60 @@ import {
     SyxForm,
 } from "./schema/flag"
 
+import {Cmd} from "./schema/cmd"
+
 // ~~~
+
+export namespace Env {
+    export const {SHORT} = SyxForm
+    export const {DASH_DASH} = Cmd
+    export const {SY_DATA} = Data
+
+    export type Uq = never
+        | typeof SHORT
+        | typeof DASH_DASH
+        | typeof SY_DATA
+}
+
+export interface Reduc {
+    tail :string
+    env :Env.Uq
+}
 
 export type Stripple = [
       string, //init sequence
       string, //short syntax form
       string] //long syntax form
+
+function shiftRel(
+    tail :string,
+    env :Env.Uq,
+) :{
+    shift :Int[]
+} {
+    const {startsWith, indexOf} = tail
+    const spaceI = indexOf(" ") as Int
+
+    if (env === SyxForm.SHORT)
+        return {
+            shift: [0 as Int, 1 as Int],
+        }
+    else
+    if (startsWith("--")) {
+        const lShift = 2 as Int
+        return {
+            shift: [lShift, spaceI - lShift as Int],
+        }
+    } else
+    if (startsWith("-")) {
+        const lShift = 1 as Int
+        return {
+            shift: [0 as Int, 9 as Int],
+        }
+    }
+
+    throw new Error("preparse: Flag expected!")
+}
 
 export default function preparse(
     args :string[],
