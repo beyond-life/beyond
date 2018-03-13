6f9a73016e2dc18fec235a8bb16f56c1ee81dfb9
probit @ 3520 <probitlabs@protonmail.com>
2018-03-13 15:42:12 +0100

    cli.poi: Formattin, Addin lost chars

--- pkg/@cli/src/char-pois.ts
+++ pkg/@cli/src/char-pois.ts
@@ -9,77 +9,94 @@ import {
 export namespace Latin {
     export const page = {
         feed: // line
-            0x0a,
+            [0x0a],
         return: // (Mac)
-            0x0d,
+            [0x0d],
     }
 
     export const white = {
         tab:
-            0x09,
+            [0x09],
         space:
-            0x20,
+            [0x20],
     }
+    
     export const punc = {
         bang: // `!`
-            0x21,
-        quote: // `"`
-            0x22,
-        apostrophe:
-            0x27,
+            [0x21],
+        comma:
+            [0x2c],
+        dot:
+            [0x2e],
+        colon:
+            [0x3a],
+        semicolon:
+            [0x3b],
+        equal:
+            [0x3d],
+        query: // `?`
+            [0x3f],
     }
     export const sign = {
         hash: // `#`
-            0x23,
+            [0x23],
         percent:
-            0x25,
+            [0x25],
         ampersand: // `&`
-            0x26,
+            [0x26],
+        asterisk:
+            [0x2a],
+        plus:
+            [0x2b],
+        minus:
+            [0x2d],
+        slash:
+            [0x2f, 0x5c],
+        angle:
+            [0x3c, 0x3e],
+        // ---
+        at:
+            [0x40],
+        pipe: //vertical bar
+            [0x7c],
+        tilde:
+            [0x7e],
+    }
+    export const diacrit = {
+        inch: // `"`
+            [0x22],
+        apostrophe:
+            [0x27],
+        // ---
+        circumflex:
+            [0x5e],
+        grave:
+            [0x60],
     }
 
     export const letter = {
         dollar:
-            0x24,
+            [0x24],
+        // ---
+        romanCap:
+            [...till(range(0x41 as Int), 0x5a as Int)],
+        lowLine:
+            [0x5f],
+        roman:
+            [...till(range(0x61 as Int), 0x7a as Int)],
+    }
+    export const digit = {
+        arabic:
+            [...till(range(0x30 as Int), 0x39 as Int)],
     }
 
-    export const bracs = {
-        parens_l:
-            0x28,
-        parens_r:
-            0x29,
+    export const brac = {
+        parens:
+            [0x28, 0x29],
+        // ---
+        bracket:
+            [0x5b, 0x5d],
+        brace:
+            [0x7b, 0x7d],
     }
-/*
-    asterisk: Kind.Sign
-        | 0x2a,
-    plus: Kind.Sign
-        | 0x2b,
-
-    comma: Kind.Punc
-        | 0x2c,
-    minus: Kind.Sign
-        | 0x2d,
-    dot: Kind.Punc
-        | 0x2e,
-    slash: Kind.Sign
-        | 0x2f,
-
-    digits_0: Kind.Num
-        | 0x30,
-    // …
-    digits_n: Spr.Range
-        | 0x39,
-    colon: Kind.Punc
-        | 0x3a,
-    semicolon = Kind.Punc
-        | 0x3b,
-
-    angle_l = Kind.Angle // `<`
-        | 0x3c,
-    equal = Kind.Punc
-        | 0x3d,
-    angle_r = Kind.Angle
-        | 0x3e,
-    query = Kind.Punc
-        | 0x3f,
-    */
 }
