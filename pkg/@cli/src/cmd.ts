f7f409b14e0e7eff97050990a8afe15c5b411335
probit @ 3520 <probitlabs@protonmail.com>
2018-03-14 13:55:56 +0100

    lowbar: Fixin memory leak → default incrementation; cli.cmd: Dependin on tokenizer

--- pkg/@cli/src/cmd.ts
+++ pkg/@cli/src/cmd.ts
@@ -1,6 +1,7 @@
-import preparse from "./preparse"
+import tok from "./parse/tok"
 
 process.stderr.write("\n> Reading following options:")
 const av = process.argv.slice(2)
-for (let a of av) process.stdout.write("\n* " + a)
-preparse(av)
+for (let a of av) process.stdout.write(`\n* "${a}"`)
+process.stderr.write("\n===")
+tok(av)
